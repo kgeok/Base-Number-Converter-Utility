c7a5027ca36f3668719b48cbc8ebe09b987d7bbe
Kevin George <17203241+kgeok@users.noreply.github.com>
2018-07-06 10:20:05 -0400

    Version 0.2
    Support for Decimal to Octal Conversion and command-line flags

--- basenumconv.cpp
+++ basenumconv.cpp
@@ -1,4 +1,4 @@
-//version 0.1
+//version 0.2
 
 #include <iostream>
 #include <string.h>
@@ -34,16 +34,54 @@ void dec2bin(unsigned long int i){
     cout << "" << endl;
 }
 
+void dec2oct(unsigned long int i){
+
+    int d = fabs(i);
+
+    base = (log(d)/log(8));
+    int buffer[base];
+    //converting base 10 to base 8
+    //We do that by contsantly dividing the input by 8 until we can no longer do so
+
+    for (int l = 0; l <= base; l++){
+
+        buffer[l] = d % 8;
+        d = d / 8;   
+        //we have a buffer to temporary store the number, then we are going to pour the contents in reverse
+    }
+
+    for (int w = 0; w<=base; w++){
+
+     cout << buffer[base-w]; 
+
+    }
+    cout << "" << endl;
+}
+
 int main(int argc, char *argv[])
 {
 
-    if (argc < 2){
+char* argu = argv[1];
+
+
+    if (argc < 3){
+        cout << "Error, no input provided, please input a non-negative decimal number" << endl;
+    }
+    if (argc > 3){
         cout << "Error, no input provided, please input a non-negative decimal number" << endl;
     }
 
-    int input = atoi(argv[1]);
+    int input = atoi(argv[2]);
 
-    dec2bin(input);
+    if  (!strcmp(argu, "-o")){
+
+        dec2oct(input);
+    }
+
+    if  (!strcmp(argu, "-b")){
+
+        dec2bin(input);
+    }
 
     return 0;
 }
