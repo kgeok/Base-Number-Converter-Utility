7a0a8ccc1fb0d19f23f20b84ccdedc4242303b80
Kevin George <17203241+kgeok@users.noreply.github.com>
2018-09-20 18:40:53 -0400

    Version 0.3

--- basenumconv.cpp
+++ basenumconv.cpp
@@ -1,4 +1,4 @@
-//version 0.2
+//version 0.3
 
 #include <iostream>
 #include <string.h>
@@ -10,16 +10,69 @@ using namespace std;
 int base;
 int bin;
 
+void oct2oct(unsigned long int i){
+    cout << i;
+}
+void bin2bin(unsigned long int i){
+    cout << i;
+}
+void bin2oct(unsigned long int i){
+    cout << i;
+}
+
+void bin2dec(unsigned long int i){
+
+int d = fabs(i);
+int result = 0;
+
+const int base = (log(d)/log(10));
+
+int buffer[base];
+
+for (int l=0; l<=base; l++){
+
+double p = l;
+
+buffer[l] = (d / pow(10.0, p));
+buffer[l] = buffer[l] % 10;
+
+}
+
+for (int y = 0; y <= base; y++) {
+
+	buffer[y] = buffer[y] * pow(2.0, y);	
+
+}
+
+    for (int w = 0; w<=base; w++){
+
+     buffer[base] = buffer[base-w]; 
+        
+
+    }
+    cout << "" << endl;
+
+for (int q = 0; q <= base; q++) {
+
+	result += buffer[q];
+
+}
+
+cout << result;
+cout << "" << endl;
+
+}
+
 void dec2bin(unsigned long int i){
 
     int d = fabs(i);
 
-    base = (log(d)/log(2));
-    int buffer[base];
+    const int base = (log(d)/log(2));
+    int buffer[base]; //uninitlized array
     //converting base 10 to base 2
     //We do that by contsantly dividing the input by 2 until we can no longer do so
 
-    for (int l = 0; l <= base; l++){
+    for (int l = 0; l <= base; l++){ //Filling the null array with values
 
         buffer[l] = d % 2;
         d = d / 2;   
@@ -38,7 +91,7 @@ void dec2oct(unsigned long int i){
 
     int d = fabs(i);
 
-    base = (log(d)/log(8));
+    const int base = (log(d)/log(8));
     int buffer[base];
     //converting base 10 to base 8
     //We do that by contsantly dividing the input by 8 until we can no longer do so
@@ -58,6 +111,12 @@ void dec2oct(unsigned long int i){
     cout << "" << endl;
 }
 
+void dec2dec(unsigned long int i){
+    cout << i;
+}
+
+
+
 int main(int argc, char *argv[])
 {
 
@@ -73,15 +132,35 @@ char* argu = argv[1];
 
     int input = atoi(argv[2]);
 
-    if  (!strcmp(argu, "-o")){
+    if  (!strcmp(argu, "-do")){
 
         dec2oct(input);
     }
 
-    if  (!strcmp(argu, "-b")){
+    if  (!strcmp(argu, "-db")){
 
         dec2bin(input);
     }
+
+	if (!strcmp(argu, "-bd")) {
+
+		bin2dec(input);
+	}
+
+    if (!strcmp(argu, "-bb")) {
+
+		bin2bin(input);
+	}
+
+    if (!strcmp(argu, "-dd")) {
+
+		dec2dec(input);
+	}
+
+    if (!strcmp(argu, "-oo")) {
+
+		oct2oct(input);
+	}
 
     return 0;
 }
